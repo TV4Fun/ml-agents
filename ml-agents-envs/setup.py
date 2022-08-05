3c537973b9ce640126a446510c3f6ed8fe9b5aef
Joel Croteau <joelcroteau@joelcroteau.com>
2022-08-05 09:42:50 -0700

    Revert "Incorporate changes from mlagents-envs 0.29.0 released on PyPI"
    This reverts commit 6aa1b5044362a686005fd79a174e12c6e2f8b966.
    This commit was supposed to be on a separate branch from main. GitHub,
    you're supposed to stop me from making mistakes like this.

--- ml-agents-envs/setup.py
+++ ml-agents-envs/setup.py
@@ -40,12 +40,11 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=find_packages(
-        exclude=["*.tests", "*.tests.*", "tests.*", "tests", "colabs", "*.ipynb"]
-    ),
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     zip_safe=False,
     install_requires=[
         "cloudpickle",
@@ -54,12 +53,7 @@ setup(
         "Pillow>=4.2.1",
         "protobuf>=3.6",
         "pyyaml>=3.1.0",
-        "gym>=0.21.0",
-        "pettingzoo>=1.15.0",
-        "numpy==1.21.2",
-        "filelock>=3.4.0",
     ],
-    python_requires=">=3.7.2,<3.10.0",
-    # TODO: Remove this once mypy stops having spurious setuptools issues.
-    cmdclass={"verify": VerifyVersionCommand},  # type: ignore
+    python_requires=">=3.6.1",
+    cmdclass={"verify": VerifyVersionCommand},
 )
