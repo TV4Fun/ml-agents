f389c7cf3160b9fe1ab91dd52ec3056fc949661b
Joel Croteau <joelcroteau@joelcroteau.com>
2022-08-04 20:37:18 -0700

    [WIP] Add experimental MPS support

--- ml-agents/mlagents/torch_utils/torch.py
+++ ml-agents/mlagents/torch_utils/torch.py
@@ -1,11 +1,11 @@
 import os
-
 from distutils.version import LooseVersion
+
 import pkg_resources
+
 from mlagents.torch_utils import cpu_utils
 from mlagents.trainers.settings import TorchSettings
 from mlagents_envs.logging_util import get_logger
-
 
 logger = get_logger(__name__)
 
@@ -32,24 +32,90 @@ assert_torch_installed()
 # This should be the only place that we import torch directly.
 # Everywhere else is caught by the banned-modules setting for flake8
 import torch  # noqa I201
+from torch.overrides import TorchFunctionMode
 
+_DEVICE_CONSTRUCTOR = {
+    # standard ones
+    torch.empty,
+    torch.empty_strided,
+    torch.empty_quantized,
+    torch.ones,
+    torch.arange,
+    torch.bartlett_window,
+    torch.blackman_window,
+    torch.eye,
+    torch.fft.fftfreq,
+    torch.fft.rfftfreq,
+    torch.full,
+    torch.fill,
+    torch.hamming_window,
+    torch.hann_window,
+    torch.kaiser_window,
+    torch.linspace,
+    torch.logspace,
+    torch.nested_tensor,
+    # torch.normal,
+    torch.ones,
+    torch.rand,
+    torch.randn,
+    torch.randint,
+    torch.randperm,
+    torch.range,
+    torch.sparse_coo_tensor,
+    torch.sparse_compressed_tensor,
+    torch.sparse_csr_tensor,
+    torch.sparse_csc_tensor,
+    torch.sparse_bsr_tensor,
+    torch.sparse_bsc_tensor,
+    torch.tril_indices,
+    torch.triu_indices,
+    torch.vander,
+    torch.zeros,
+    torch.asarray,
+    # weird ones
+    torch.tensor,
+    torch.as_tensor,
+}
 
 torch.set_num_threads(cpu_utils.get_num_threads_to_use())
 os.environ["KMP_BLOCKTIME"] = "0"
 
 
+class DeviceMode(TorchFunctionMode):
+    def __init__(self, device):
+        self.device = torch.device(device)
+
+    def __torch_function__(self, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        if func in _DEVICE_CONSTRUCTOR and 'device' not in kwargs:
+            kwargs['device'] = self.device
+        return func(*args, **kwargs)
+
+
 _device = torch.device("cpu")
+_device_mode = None
 
 
 def set_torch_config(torch_settings: TorchSettings) -> None:
     global _device
 
     if torch_settings.device is None:
-        device_str = "cuda" if torch.cuda.is_available() else "cpu"
+        if torch.cuda.is_available():
+            device_str = "cuda"
+        elif torch.backends.mps.is_available():
+            device_str = "mps"
+        else:
+            device_str = "cpu"
     else:
         device_str = torch_settings.device
 
     _device = torch.device(device_str)
+
+    if _device.type == "mps":
+        global _device_mode
+        _device_mode = DeviceMode.push(_device)
+        _device_mode.__enter__()
 
     if _device.type == "cuda":
         torch.set_default_tensor_type(torch.cuda.FloatTensor)
